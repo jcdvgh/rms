5e745c935b21b3f48c3de6bc524006035a43f19c
jcdvgh <152811251+jcdvgh@users.noreply.github.com>
2023-12-04 02:14:23 +0530

    setup.sh

--- setup.sh
+++ setup.sh
@@ -8,32 +8,15 @@ source env/bin/activate  # For Linux/macOS
 env\Scripts\activate    # For Windows
 
 # Install the required packages
+pip install -r nltk.txt
 pip install -r requirements.txt
 pip install --upgrade streamlit
 -m pip install --upgrade pip
-pip install streamlit
-pip install requests
-pip install bs4
-pip install BeautifulSoup
-pip install pandas
-pip install pd
-pip install time
 pip install nltk
+pip install pandas
 pip install numpy
-pip install np
-pip install nltk.sentiment 
-pip install SentimentIntensityAnalyzer
 pip install seaborn
-pip install sns
-pip install matplotlib.pyplot 
-pip install plt
-pip install matplotlib.gridspec 
-pip install gridspec
-pip install matplotlib.dates 
-pip install mdates
-pip install matplotlib.backends.backend_pdf 
-pip install PdfPages
-pip install wordcloud 
-pip install WordCloud
-pip install dateutil.relativedelta 
-pip install relativedelta
+pip install matplotlib
+pip install wordcloud
+pip install python-dateutil
+pip install streamlit
