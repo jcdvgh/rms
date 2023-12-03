8cc737a0daa3d267f546b92d2b30b26d37d201c6
jcdvgh <152811251+jcdvgh@users.noreply.github.com>
2023-12-04 03:19:20 +0530

    rm.py

--- rm.py
+++ rm.py
@@ -2,13 +2,10 @@ import streamlit as st
 import pandas as pd
 import nltk
 import numpy as np
-from nltk.sentiment import SentimentIntensityAnalyzer
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import matplotlib.dates as mdates
-from matplotlib.backends.backend_pdf import PdfPages
-from wordcloud import WordCloud
 from dateutil.relativedelta import relativedelta
 
 # Check if NLTK data is downloaded, if not, download it
