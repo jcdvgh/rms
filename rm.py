e2061356d45e0d143095364a2b101dba25388927
jcdvgh <152811251+jcdvgh@users.noreply.github.com>
2023-12-04 06:24:02 +0530

    rm.py

--- rm.py
+++ rm.py
@@ -97,7 +97,7 @@ def render_charts(df):
 
     # --- Chart 7: Sentiment Analysis per Feedback Category ---
     st.subheader("Sentiment Analysis per Feedback Category")
-    fig7, (ax_chart, ax_text) = plt.subplots(nrows=2, figsize=(16, 20), gridspec_kw={'height_ratios': [3, 1]})
+    fig7, (ax_chart, ax_text) = plt.subplots(nrows=2, figsize=(48, 60), gridspec_kw={'height_ratios': [3, 1]})
     sns.barplot(x='review_text', y='compound_sentiment', data=df, ax=ax_chart, palette='viridis')
     ax_chart.set_xlabel('Feedback Category')
     ax_chart.set_ylabel('Average Compound Sentiment')
